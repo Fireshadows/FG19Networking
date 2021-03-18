6ff2069b159748ac10be0ddccd285180ba37a8c3
Fireshadows <antonioh.ackalin@hotmail.com>
2021-03-18 16:52:19 +0100

    Assignment upload

---
+++ Source/FGNet/FGmovementStatics.cpp
@@ -0,0 +1,18 @@
+#include "FGmovementStatics.h"
+#include "GameFramework/Actor.h"
+#include "Components/SceneComponent.h"
+
+FFGFrameMovement::FFGFrameMovement(AActor* InActor)
+{
+	StartLocation = InActor->GetActorLocation();
+}
+
+FFGFrameMovement::FFGFrameMovement(USceneComponent* InSceneComponent)
+{
+	StartLocation = InSceneComponent->GetComponentLocation();
+}
+
+void FFGFrameMovement::AddDelta(const FVector& InDelta)
+{
+	MovementDelta += InDelta;
+}
